58b01e37fcf74a43b10500ddb3c65006de521ea0
Ryockans <pavekon99@yandex.com>
2022-09-21 15:10:30 +0300

    made collapse

--- src/js/code.js
+++ src/js/code.js
@@ -1,1 +1,24 @@
+const basic = document.querySelector(".collapse.-basic");
+const accordion = document.querySelector(".collapse.-accordion");
 
+basic.addEventListener("click", toggleTab);
+accordion.addEventListener("click", function (event) {
+  if (this.querySelector(".-show") !== null) {
+    for (let item of this.querySelectorAll(".-show")) {
+      if (
+        item.closest(".collapse__item") !==
+        event.target.closest(".collapse__item")
+      ) {
+        item.classList.remove("-show");
+      }
+    }
+  }
+  toggleTab(event);
+});
+
+function toggleTab(event) {
+  const element = event.target.closest(".collapse__item");
+
+  element.querySelector(".collapse__toggle").classList.toggle("-show");
+  element.querySelector(".collapse__text").classList.toggle("-show");
+}
