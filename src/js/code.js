298ca67b7b49aa00a90835ab27a3e8490486f5b0
Ryockans <pavekon99@yandex.com>
2022-09-28 17:46:26 +0300

    remade

--- src/js/code.js
+++ src/js/code.js
@@ -1,26 +1,55 @@
-const basic = document.querySelector(".collapse.-basic");
-const accordion = document.querySelector(".collapse.-accordion");
-
-basic.addEventListener("click", toggleTab);
-accordion.addEventListener("click", function (event) {
-  if (this.querySelector(".-show") !== null) {
-    for (let item of this.querySelectorAll(".-show")) {
-      if (
-        item.closest(".collapse__item") !==
-        event.target.closest(".collapse__item")
-      ) {
-        item.classList.remove("-show");
-      }
-    }
+class Collapse {
+  constructor(elementName, accordion = false) {
+    this.element = document.querySelector('.collapse.-' + elementName);
+    this.tabs = this.element.querySelectorAll('.collapse__item');
+    this.accordion = accordion;
+    this.activeTabs = [];
+    Collapse.AllCollapses.push(this);
   }
-  toggleTab(event);
-});
 
-function toggleTab(event) {
-  console.dir(event.target);
-  if (event.target.classList.contains('collapse__text')) return;
-  const element = event.target.closest(".collapse__item");
+  static AllCollapses = [];
 
-  element.querySelector(".collapse__toggle").classList.toggle("-show");
-  element.querySelector(".collapse__text").classList.toggle("-show");
+  openTab(tab) {
+    const tabText = tab.querySelector('.collapse__text');
+    const tabButton = tab.querySelector('.collapse__toggle');
+
+    if (this.accordion) {
+      this.activeTabs.forEach((item) => this.closeTab(item));
+    }
+
+    tabText.classList.add('-toggled');
+    tabButton.classList.add('-toggled');
+
+    this.activeTabs.push(tab);
+  }
+
+  closeTab(tab) {
+    const tabText = tab.querySelector('.collapse__text');
+    const tabButton = tab.querySelector('.collapse__toggle');
+    const tabIndex = this.activeTabs.indexOf(tab);
+
+    tabText.classList.remove('-toggled');
+    tabButton.classList.remove('-toggled');
+
+    this.activeTabs.splice(tabIndex, 1);
+  }
+
+  handleEvent(event) {
+    const isTabClick = (event.type === 'click' && targetTab !== null);
+    const targetTab = event.target.closest('.collapse__item');
+    const isLocal = Array.from(this.tabs).includes(targetTab);
+
+    if (!isLocal || !isTabClick) return;
+
+    if (this.activeTabs.includes(targetTab)) {
+      this.closeTab(targetTab)
+    } else this.openTab(targetTab);
+  }
+}
+
+new Collapse('basic');
+new Collapse('accordion', true);
+
+for (let item of Collapse.AllCollapses) {
+  item.element.addEventListener('click', item);
 }
